bd1c3343bff6c887f1b8147c698f960d8c05ae72
mishrarohit10 <mishrarohit316x@gmail.com>
2024-06-09 23:48:36 +0530

    added react icons

--- portfolio-react/src/Components/home.tsx
+++ portfolio-react/src/Components/home.tsx
@@ -2,6 +2,9 @@ import hoverprofilepic from '../Assests/pexels-hristo-fidanov-1252873.jpg';
 import { useState } from 'react';
 import profilepic from '../Assests/eren-yeager.webp';
 import { Visitors } from './visitors';
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { IoLogoGithub } from "react-icons/io5";
 
 export function Home() {
     const [hoverd, setHoverd] = useState(profilepic);
@@ -26,6 +29,14 @@ export function Home() {
                         onMouseEnter={isHovering}
                         onMouseLeave={isNotHovering}
                     />
+                </div>
+                
+                <div className='home-icon-container'>
+                    <div className='home-icons'>
+                        <FaLinkedin />
+                        <FaSquareXTwitter />
+                        <IoLogoGithub />
+                    </div>
                 </div>
             </div>
         </>
