9053f8784b72f6bc13cad3cf4b81fa03fbf4b839
rohit <mishrarohit316x@gmail.com>
2024-08-11 22:27:38 +0530

    removed about title

--- portfolio-react/src/Components/about.tsx
+++ portfolio-react/src/Components/about.tsx
@@ -1,43 +1,48 @@
 import StackIcon from "tech-stack-icons";
 
 export function About() {
-    return (
-        <div className="about" id="about">
-            <div className="about-container">
-                
-                <div className="about-title">
-                    <h1>About Me</h1>
-                </div>
-                <StackIcon name="js" />
-                <StackIcon name="reactjs" />
-                <StackIcon name="node" />
-                <StackIcon name="express" />
-                <StackIcon name="mongodb" />
-                <StackIcon name="postgresql" />
-                <StackIcon name="java" />
-                <StackIcon name="spring" />
-                <StackIcon name="go" />
-                <StackIcon name="python" />
-                <StackIcon name="typescript" />
-                <StackIcon name="git" />
-                <StackIcon name="github" />
-                <StackIcon name="docker" />
-                <StackIcon name="aws" />
-                <StackIcon name="azure" />
-                <StackIcon name="vscode" />
-                <StackIcon name="postman" />
-                <StackIcon name="gitlab" />
-                <StackIcon name="linux" />
-                <StackIcon name="nodejs" />
-                <StackIcon name="npm" />
-                <StackIcon name="swagger" />
+  return (
+    <div className="about" id="about">
+      <div className="about-container">
+        {/* <div className="about-title">
+          <h1>About Me</h1>
+        </div> */}
+        <StackIcon name="js" />
+        <StackIcon name="reactjs" />
+        <StackIcon name="node" />
+        <StackIcon name="express" />
+        <StackIcon name="mongodb" />
+        <StackIcon name="postgresql" />
+        <StackIcon name="java" />
+        <StackIcon name="spring" />
+        <StackIcon name="go" />
+        <StackIcon name="python" />
+        <StackIcon name="typescript" />
+        <StackIcon name="git" />
+        <StackIcon name="github" />
+        <StackIcon name="docker" />
+        <StackIcon name="aws" />
+        <StackIcon name="azure" />
+        <StackIcon name="vscode" />
+        <StackIcon name="postman" />
+        <StackIcon name="gitlab" />
+        <StackIcon name="linux" />
+        <StackIcon name="nodejs" />
+        <StackIcon name="npm" />
+        <StackIcon name="swagger" />
 
-                <div className="about-content">
-                    <p>
-                        I am a full stack web developer with a passion for creating beautiful, responsive web applications. I am proficient in HTML, CSS, JavaScript, and React on the front-end, and Node.js, Express, and MongoDB, PostgreSQL, java, Springboot, golang, gin on the back-end. I am also familiar with Python, fastapi and Django. I am a lifelong learner and I am always looking to improve my skills and learn new technologies.
-                    </p>
-                </div>
-            </div>
+        <div className="about-content">
+          <p>
+            I am a full stack web developer with a passion for creating
+            beautiful, responsive web applications. I am proficient in HTML,
+            CSS, JavaScript, and React on the front-end, and Node.js, Express,
+            and MongoDB, PostgreSQL, java, Springboot, golang, gin on the
+            back-end. I am also familiar with Python, fastapi and Django. I am a
+            lifelong learner and I am always looking to improve my skills and
+            learn new technologies.
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
